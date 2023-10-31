d40edfcd9da313157bb392b242df389398e9b3b5
DomiTheKava <78154813+DomiTheKava@users.noreply.github.com>
2023-10-31 10:34:38 -0500

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+/* https://www.w3schools.com/js/tryit.asp?filename=tryjs_intro_lightbulb */
+
+
+/* change image */
+
+function change(picture){
+    document.getElementById("large").src = picture;
+}
+
+
